3fd7c81564c0676f75419011d8bac2ce3d8d55b5
Q-Leito <q_leito@live.nl>
2016-06-17 11:26:23 +0200

    Initial commit

---
+++ src/utils/Constants.java
@@ -0,0 +1,16 @@
+package utils;
+
+/**
+ * Created by Quincy on 6/17/2016.
+ */
+public class Constants {
+
+    public final static double APP_WIDTH = 960.0d;
+    public final static double APP_HEIGHT = 600.0d;
+    public final static String APP_NAME = "Kmeans";
+
+    private final static String FXML_PATH = "/fxml";
+    public final static String LOGIN_FXML_PATH = String.format("%s/home.fxml", FXML_PATH);
+    public final static String LOGIN_SCENE_HEADER = "Clustering";
+}
+
